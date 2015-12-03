18a3ce7e935bba0000737819c6b91b937826fd6e
Ghosdak <tonis0812@g.ncu.edu.tw>
2015-12-03 22:50:39 +0800

    Homework1

--- app.js
+++ app.js
@@ -1,31 +1,3 @@
-window.setTimeout("Tick()", 1000);
-
-function Tick() {
-window.setTimeout("Tick()", 1000);
-}
- 
-var Timer;
-var TotalSeconds;
-
-
-function CreateTimer(TimerID, Time) {
-Timer = document.getElementById(TimerID);
-TotalSeconds = Time;
-
-UpdateTimer()
-window.setTimeout("Tick()", 1000);
-}
-
-function Tick() {
-TotalSeconds -= 1;
-UpdateTimer()
-window.setTimeout("Tick()", 1000);
-}
-
-function UpdateTimer() {
-Timer.innerHTML = TotalSeconds;
-}
-function startTime() {
-    var today = new Date();
-    document.getElementById('txt').innerHTML = today
-    var t = setTimeout(startTime, 500);
+window.addEventListener("load", function() {
+  console.log("Hello World!");
+});
